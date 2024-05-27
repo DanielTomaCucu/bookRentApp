2f34e67c95bd4ebe220489db573946ea7d9536b7
cucutomadaniel <cucutomadaniel@gmail.com>
2024-05-27 15:04:06 +0300

    solve bugs in books implementation

--- book.rent/src/main/java/com/book/rent/book/BookController.java
+++ book.rent/src/main/java/com/book/rent/book/BookController.java
@@ -4,6 +4,7 @@ import com.book.rent.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Tag(name = "Book")
 public class BookController {
-
+@Autowired
     private final BookService bookService;
 
     @PostMapping
--- book.rent/src/main/java/com/book/rent/history/BookTransactionHistoryRepository.java
+++ book.rent/src/main/java/com/book/rent/history/BookTransactionHistoryRepository.java
@@ -4,20 +4,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookTransactionHistoryRepository extends JpaRepository<BookTransactionHistory, Integer> {
-@Query("""
-        SELECT history
-        FROM bookTransactionHistory history
-        WHERE history.user.id =:userId
-        """)
-    Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Integer userId);
 
     @Query("""
         SELECT history
-        FROM bookTransactionHistory history
-        WHERE history.book.owner.id =:userId
+        FROM BookTransactionHistory history
+        WHERE history.user.id = :userId
         """)
-    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Integer userId);
+    Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, @Param("userId") Integer userId);
 
+    @Query("""
+        SELECT history
+        FROM BookTransactionHistory history
+        WHERE history.book.owner.id = :userId
+        """)
+    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, @Param("userId") Integer userId);
 }
